7e44c07ff1dc9ce2784f40bb99221886cc34b8a9
HarshitNischal <71174516+HarshitNischal@users.noreply.github.com>
2021-07-24 16:01:39 +0530

    Add files via upload

---
+++ screens/StarMap.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class StarMap extends Component{
+    render(){
+        return (
+            <Text>Star Map</Text>
+        )
+    }
+}
